ad0734516cdd3ebe178622981ff3abc728d492aa
wallematthias <matthias.walle@gmail.com>
2023-03-10 10:47:09 +0100

    Update aim.py

--- helpers/aim.py
+++ helpers/aim.py
@@ -30,32 +30,58 @@ class AIMFile:
         self.position = (0, 0, 0) if position is None else position
 
 
-def load_aim(filepath):
+def load_aim(filepath: str) -> AIMFile:
+    """
+    Load an AIM file and convert it into a standardized format.
+
+    Args:
+        filepath (str): The path to the AIM file that needs to be loaded.
+
+    Returns:
+        AIMFile: An object that contains the image data in a standardized format.
+
+    Raises:
+        Exception: If the file cannot be loaded.
+
+    """
   
     try:
+        # Try to read the file as an image file
         image = itk.imread(filepath)
 
+        # Convert the image to a NumPy array and transpose it to change the axis order
         arr = np.transpose(np.asarray(image), (1, 2, 0))
 
+        # Get the calibration constants from the processing log and use them to convert the Hounsfield units to density values
         mu_scaling, hu_mu_water, hu_mu_air, density_slope, density_intercept = get_aim_calibration_constants_from_processing_log(filepath)
-        density = convert_hounsfield_to_mgccm(arr,
-            mu_scaling, hu_mu_water, hu_mu_air, density_slope, density_intercept)
-        data= Quantity(density,'mg/cm**3')
+        density = convert_hounsfield_to_mgccm(arr, mu_scaling, hu_mu_water, hu_mu_air, density_slope, density_intercept)
 
-        processing_log= dict(image)
+        # Convert the density values to a Quantity object with units of mg/cm^3
+        data= Quantity(density, 'mg/cm**3')
+
+        # Create a dictionary containing the processing log and add the density slope and intercept to it
+        processing_log = dict(image)
         processing_log['density_slope'] = density_slope
         processing_log['density_intercept'] = density_intercept
 
     except:
+        # If the file cannot be read as an image file, assume it is a binary mask file
         print('Reading mask Data')
-        image = itk.imread(filepath,itk.UC)
-        data= Quantity(np.transpose(np.asarray(image)>0, (1, 2, 0)),'dimensionless')
-        processing_log= dict(image)
+        image = itk.imread(filepath, itk.UC)
+        
+        # Convert the mask data to a Quantity object with units of 'dimensionless'
+        data= Quantity(np.transpose(np.asarray(image) > 0, (1, 2, 0)), 'dimensionless')
+        
+        # Create a dictionary containing the processing log
+        processing_log = dict(image)
 
-    voxelsize= Quantity(processing_log['spacing'],'mm')
-    position= np.round(processing_log['origin']/processing_log['spacing']).astype(int)
+    # Extract the voxel size and position from the processing log and create a Quantity object for the voxel size
+    voxelsize = Quantity(processing_log['spacing'], 'mm')
+    position = np.round(processing_log['origin'] / processing_log['spacing']).astype(int)
 
+    # Return an AIMFile object with the data, processing log, voxel size, and position
     return AIMFile(data, processing_log, voxelsize, position)
+
 
 def write_aim(aim_file, file_path):
   
