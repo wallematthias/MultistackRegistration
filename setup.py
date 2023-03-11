5cc9719532f43f7da6dbc01fbad1d89046c333af
wallematthias <92020703+wallematthias@users.noreply.github.com>
2023-03-11 17:09:36 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='multistack_registration',
+    version='1.0.0',
+    author='Matthias Walle',
+    author_email='matthias.walle@hest.ethz.ch',
+    description='Multistack registration for HR-pQCT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wallematthias/multistack_registration',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
